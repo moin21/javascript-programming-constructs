301ebeaf994ee766cbdcbbd43ca6d88913f31c85
Moinuddin Asraf <mamoinuddin@gmail.com>
2022-04-26 02:29:34 +0530

    Refactor: Logic Correction

--- selection-practice-problem-for-loop/PrimeFactorization.js
+++ selection-practice-problem-for-loop/PrimeFactorization.js
@@ -3,20 +3,11 @@ const prompt = require("prompt-sync")();
 const number = Number(prompt("Enter Number: "));
 
 function primeFactorization(number) {
-  while (number % 2 == 0) {
-    process.stdout.write(2 + " ");
-    number /= 2;
-  }
-  for (let i = 2; i * i <= number; i++) {
-    while (number % i == 0) {
-      if (number % i == 0) {
-        process.stdout.write(i + " ");
-        number /= i;
-      }
-    }
-  }
-  if (number > 2) {
-    console.log(number);
-  }
-}
+  for (int i = 2; i <= number; i++) {
+			if(number%i==0) {
+				System.out.print(i+" ");
+				number = number/i;
+			}
+		}
+	}
 primeFactorization(number);
